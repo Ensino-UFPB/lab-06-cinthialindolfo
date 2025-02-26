967daf7dcb7d32d6fb5aa2f99517db573e14aa1c
João Silva <cinthia20lindolfo@gmail.com>
2025-02-26 14:33:02 -0300

    subindo o código

--- js/validacao.js
+++ js/validacao.js
@@ -1,25 +1,9 @@
-export function valida(input) {
-    const tipoDeInput = input.dataset.tipo
-
-    if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
-    }
-
-    if(input.validity.valid) {
-        input.parentElement.classList.remove('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
-    } else {
-        input.parentElement.classList.add('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
-    }
-}
-
 const tiposDeErro = [
     'valueMissing',
     'typeMismatch',
     'patternMismatch',
     'customError'
-]
+];
 
 const mensagensDeErro = {
     nome: {
@@ -39,7 +23,7 @@ const mensagensDeErro = {
     },
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
-        customError: 'O CPF digitado não é válido.' 
+        customError: 'O CPF digitado não é válido.'
     },
     cep: {
         valueMissing: 'O campo de CEP não pode estar vazio.',
@@ -54,53 +38,92 @@ const mensagensDeErro = {
     },
     estado: {
         valueMissing: 'O campo de estado não pode estar vazio.'
+    },
+    telefone: {
+        valueMissing: 'O campo de telefone não pode estar vazio.',
+        customError: 'O telefone digitado não é válido.'
+    },
+    instagram: {
+        valueMissing: 'O campo de Instagram não pode estar vazio.',
+        customError: 'O Instagram deve começar com @.'
+    },
+    nomeProduto: {
+        valueMissing: 'O campo de nome do produto não pode estar vazio.'
+    },
+    preco: {
+        valueMissing: 'O campo de preço não pode estar vazio.',
+        customError: 'O preço digitado não é válido.'
+    },
+    quantidade: {
+        valueMissing: 'O campo de quantidade não pode estar vazio.',
+        customError: 'A quantidade deve ser um número válido.'
     }
-}
+};
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input),
-    cpf:input => validaCPF(input),
-    cep:input => recuperarCEP(input)
+    dataNascimento: input => validaDataNascimento(input),
+    cpf: input => validaCPF(input),
+    cep: input => recuperarCEP(input),
+    telefone: input => validaTelefone(input),
+    instagram: input => validaInstagram(input),
+    nomeProduto: input => validaNomeProduto(input),
+    preco: input => validaPreco(input),
+    quantidade: input => validaQuantidade(input)
+};
+
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+    } else {
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+    }
 }
 
 function mostraMensagemDeErro(tipoDeInput, input) {
-    let mensagem = ''
+    let mensagem = '';
     tiposDeErro.forEach(erro => {
-        if(input.validity[erro]) {
-            mensagem = mensagensDeErro[tipoDeInput][erro]
+        if (input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro];
         }
-    })
-    
-    return mensagem
+    });
+    return mensagem;
 }
 
 function validaDataNascimento(input) {
-    const dataRecebida = new Date(input.value)
-    let mensagem = ''
+    const dataRecebida = new Date(input.value);
+    let mensagem = '';
 
-    if(!maiorQue18(dataRecebida)) {
-        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    if (!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.';
     }
 
-    input.setCustomValidity(mensagem)
+    input.setCustomValidity(mensagem);
 }
 
 function maiorQue18(data) {
-    const dataAtual = new Date()
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
 
-    return dataMais18 <= dataAtual
+    return dataMais18 <= dataAtual;
 }
 
 function validaCPF(input) {
-    const cpfFormatado = input.value.replace(/\D/g, '')
-    let mensagem = ''
+    const cpfFormatado = input.value.replace(/\D/g, '');
+    let mensagem = '';
 
-    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
-        mensagem = 'O CPF digitado não é válido.'
+    if (!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.';
     }
 
-    input.setCustomValidity(mensagem)
+    input.setCustomValidity(mensagem);
 }
 
 function checaCPFRepetido(cpf) {
@@ -115,83 +138,145 @@ function checaCPFRepetido(cpf) {
         '77777777777',
         '88888888888',
         '99999999999'
-    ]
-    let cpfValido = true
+    ];
+    let cpfValido = true;
 
     valoresRepetidos.forEach(valor => {
-        if(valor == cpf) {
-            cpfValido = false
+        if (valor == cpf) {
+            cpfValido = false;
         }
-    })
+    });
 
-    return cpfValido
+    return cpfValido;
 }
 
 function checaEstruturaCPF(cpf) {
-    const multiplicador = 10
+    const multiplicador = 10;
 
-    return checaDigitoVerificador(cpf, multiplicador)
+    return checaDigitoVerificador(cpf, multiplicador);
 }
 
 function checaDigitoVerificador(cpf, multiplicador) {
-    if(multiplicador >= 12) {
-        return true
+    if (multiplicador >= 12) {
+        return true;
     }
 
-    let multiplicadorInicial = multiplicador
-    let soma = 0
-    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
-    const digitoVerificador = cpf.charAt(multiplicador - 1)
-    for(let contador = 0; multiplicadorInicial > 1 ; multiplicadorInicial--) {
-        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
-        contador++
+    let multiplicadorInicial = multiplicador;
+    let soma = 0;
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+    const digitoVerificador = cpf.charAt(multiplicador - 1);
+    for (let contador = 0; multiplicadorInicial > 1 ; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial;
+        contador++;
     }
 
-    if(digitoVerificador == confirmaDigito(soma)) {
-        return checaDigitoVerificador(cpf, multiplicador + 1)
+    if (digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1);
     }
 
-    return false
+    return false;
 }
 
 function confirmaDigito(soma) {
-    return 11 - (soma % 11)
+    return 11 - (soma % 11);
 }
 
 function recuperarCEP(input) {
-    const cep = input.value.replace(/\D/g, '')
-    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const cep = input.value.replace(/\D/g, '');
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
     const options = {
         method: 'GET',
         mode: 'cors',
         headers: {
             'content-type': 'application/json;charset=utf-8'
         }
-    }
+    };
 
-    if(!input.validity.patternMismatch && !input.validity.valueMissing) {
-        fetch(url,options).then(
+    if (!input.validity.patternMismatch && !input.validity.valueMissing) {
+        fetch(url, options).then(
             response => response.json()
         ).then(
             data => {
-                if(data.erro) {
-                    input.setCustomValidity('Não foi possível buscar o CEP.')
-                    return
+                if (data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP.');
+                    return;
                 }
-                input.setCustomValidity('')
-                preencheCamposComCEP(data)
-                return
+                input.setCustomValidity('');
+                preencheCamposComCEP(data);
+                return;
             }
-        )
+        );
     }
 }
 
 function preencheCamposComCEP(data) {
-    const logradouro = document.querySelector('[data-tipo="logradouro"]')
-    const cidade = document.querySelector('[data-tipo="cidade"]')
-    const estado = document.querySelector('[data-tipo="estado"]')
+    const logradouro = document.querySelector('[data-tipo="logradouro"]');
+    const cidade = document.querySelector('[data-tipo="cidade"]');
+    const estado = document.querySelector('[data-tipo="estado"]');
 
-    logradouro.value = data.logradouro
-    cidade.value = data.localidade
-    estado.value = data.uf
+    logradouro.value = data.logradouro;
+    cidade.value = data.localidade;
+    estado.value = data.uf;
+}
+
+function validaTelefone(input) {
+    const telefone = input.value.replace(/\D/g, '');
+    let mensagem = '';
+
+    if (!telefone) {
+        mensagem = mensagensDeErro.telefone.valueMissing;
+    } else if (telefone.length !== 11) {
+        mensagem = 'O telefone deve conter 11 dígitos (DDD + número).';
+    } else if (!/^\d{2}/.test(telefone)) {
+        mensagem = 'O DDD do telefone não é válido.';
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function validaInstagram(input) {
+    const instagram = input.value;
+    let mensagem = '';
+
+    if (!instagram) {
+        mensagem = mensagensDeErro.instagram.valueMissing;
+    } else if (!instagram.startsWith('@')) {
+        mensagem = mensagensDeErro.instagram.customError;
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function validaNomeProduto(input) {
+    let mensagem = '';
+
+    if (!input.value) {
+        mensagem = mensagensDeErro.nomeProduto.valueMissing;
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function validaPreco(input) {
+    let mensagem = '';
+
+    if (!input.value) {
+        mensagem = mensagensDeErro.preco.valueMissing;
+    } else if (isNaN(input.value.replace(/[^0-9]/g, ''))) {
+        mensagem = mensagensDeErro.preco.customError;
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function validaQuantidade(input) {
+    let mensagem = '';
+
+    if (!input.value) {
+        mensagem = mensagensDeErro.quantidade.valueMissing;
+    } else if (isNaN(input.value)) {
+        mensagem = mensagensDeErro.quantidade.customError;
+    }
+
+    input.setCustomValidity(mensagem);
 }
